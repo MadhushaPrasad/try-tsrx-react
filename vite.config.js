297a9168a10e8ea0f10f341117e48b6f3f8a2580
MadhushaPrasad <madushaprasad21@gmail.com>
2026-05-07 12:26:30 +0530

    feat: update dependencies and refactor App component structure

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import tsrxReact from "@tsrx/vite-plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [tsrxReact(), tailwindcss()],
+});
