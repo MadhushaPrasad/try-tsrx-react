cc510c91dc839bcdfea24e5b1318a9b01a023b34
MadhushaPrasad <madushaprasad21@gmail.com>
2026-05-08 13:25:00 +0530

    feat: add NavBar component and integrate it into App

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import NavBar from "./components/NavBar.tsrx"
+
+function App() {
+
+  return (
+    <>
+    <h1 className='text-red-500'>Hello</h1>
+    <NavBar/>
+    </>
+  )
+}
+
+export default App
